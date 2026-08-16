74c7ef4d2d2904961dabda53ad28880aaebd1cac
mohamed ahmed <bbcvsmsn123@gmail.com>
2026-08-17 01:22:58 +0300

    phase 1

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 //starting the server (local host)
 app.listen(port,()=>{
     console.log(`Server is running on port ${PORT}`);
